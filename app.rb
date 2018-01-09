429ecd3c7a67f1c6183ebafd4d48d30f31b65298
Sherry El Garhy <sherryptk@gmail.com>
2018-01-09 00:21:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,6 +30,14 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
+  get "/:operation/:number1/:number2" do
+    # @operation = params[:operation]
+    # @number1 = params[:number1].to_i
+    # @number2 = params[:number2].to_i
+    @result =  params[:number1].to_i params[:operation].to_sym params[:number2].to_i
+    "#{@result}"
+  end
+  
 
 
 end
